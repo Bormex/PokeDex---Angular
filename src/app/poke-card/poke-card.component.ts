a2877e05d029afbbcb3f5a7896ce6246f9e2b5a5
Niclas Holzhey <niclas@ImStandingBehindU.local>
2025-11-26 23:29:54 +0100

    changed the method to get img and name of each evolution

--- src/app/poke-card/poke-card.component.ts
+++ src/app/poke-card/poke-card.component.ts
@@ -5,7 +5,7 @@ import { map, Observable, switchMap } from 'rxjs';
 @Component({
   selector: 'app-poke-card',
   imports: [],
-  template:`  
+  template:`
   <h1>Pokemon Cards Down below</h1>
     <main id="pokemons">
       <div id="pokemon">
@@ -25,6 +25,7 @@ import { map, Observable, switchMap } from 'rxjs';
 
 export class PokeCardComponent implements OnInit, OnDestroy {
 
+
   constructor (private pokeApi: PokeApiService) { }
 
   subPokemon: any;
@@ -32,7 +33,7 @@ export class PokeCardComponent implements OnInit, OnDestroy {
 
   items: string[] | undefined;
 
-  elementColor = {         
+  elementColor = {
     bug: "#92BC2C",
     dark: "#595761",
     dragon: "#0C69C8",
@@ -83,29 +84,29 @@ export class PokeCardComponent implements OnInit, OnDestroy {
 
   pokemonCardInterface(pokeDetails: any) {
     //////////////////////// console.log(pokeDetails);
-    
+
     // this.pokemon = {
     //   name: pokeDetails.name[0].toUpperCase() + pokeDetails.name.slice(1, pokeDetails.name.length),
     //   index: pokeDetails.id.toString().padStart(4, "0"),
     //   img: pokeDetails.sprites.other['official-artwork'].front_default,
     //   height: (pokeDetails.height/10).toString() + "m",
     //   weight: pokeDetails.weight.toString().slice(0, pokeDetails.weight.toString().length - 1) + "." +  pokeDetails.weight.toString()[pokeDetails.weight.toString().length - 1] + "kg",
-    //   abilities: pokeDetails.abilities.map((pokemon: { ability: { name: string; }; }) => pokemon.ability.name[0].toUpperCase() + pokemon.ability.name.slice(1, pokemon.ability.name.length)), 
+    //   abilities: pokeDetails.abilities.map((pokemon: { ability: { name: string; }; }) => pokemon.ability.name[0].toUpperCase() + pokemon.ability.name.slice(1, pokemon.ability.name.length)),
     //   elements: pokeDetails.types.map((pokemon: { type: { name: any; }; }) => pokemon.type.name[0].toUpperCase() + pokemon.type.name.slice(1, pokemon.type.name.length)),
     //   evolutions: "",
     // }
-    
+
     // console.log("Evolutions:", this.pokemon.evolutions);
-    
+
   }
-  
+
   getPokemonEvolutions(pokemonName: any) {
     console.log("Evo Chain Object:", pokemonName);
-    
+
     console.log("Name:", pokemonName.chain.species.name);
     console.log("Url:", pokemonName.chain.species.url);
     console.log("ID:", pokemonName.chain.species.url.split("pokemon-species/")[1].slice(0, pokemonName.chain.species.url.split("pokemon-species/")[1].length - 1));
-    
+
   }
 
   //
@@ -113,27 +114,43 @@ export class PokeCardComponent implements OnInit, OnDestroy {
   //
   pokenum = 1;
 
-  pokemon: { name: any; evolutions: void; } | undefined;
 
+  pokemon: { name: any; index: any; img: any; height: string; weight: string; abilities: any; elements: any; evolutions: any; } | undefined;
 
   getPokemonWithEvolution(id: number) {
   this.pokeApi.fetchPokemonData(`pokemon/${id}`).pipe(
-    switchMap(pokemon =>
+    switchMap(pokemonData =>
       this.pokeApi.fetchPokemonData(`pokemon-species/${id}`).pipe(
-        map(species => ({ pokemon, species }))
+        map(species => ({ pokemonData, species }))
       )
     ),
-    switchMap(({ pokemon, species }) =>
+    switchMap(({ pokemonData, species }) =>
       this.pokeApi.fetchPokemonData(species.evolution_chain.url.split("https://pokeapi.co/api/v2/")[1]).pipe(
-        map(evolutionChain => ({ pokemon, evolutionChain }))
+        map(evolutionChain => ({ pokemonData, evolutionChain }))
       )
     )
   ).subscribe({
-    next: ({ pokemon, evolutionChain }) => {
-     
-      console.log(pokemon.name);
-      console.log(this.extractEvolutions(evolutionChain.chain));
+    next: ({ pokemonData, evolutionChain }) => {
 
+      console.log(pokemonData);
+
+
+    this.pokemon = {
+      name: pokemonData.name[0].toUpperCase() + pokemonData.name.slice(1, pokemonData.name.length),
+      index: pokemonData.id.toString().padStart(4, "0"),
+      img: pokemonData.sprites.other['official-artwork'].front_default,
+      height: (pokemonData.height/10).toString() + "m",
+      weight: pokemonData.weight.toString().slice(0, pokemonData.weight.toString().length - 1) + "." +  pokemonData.weight.toString()[pokemonData.weight.toString().length - 1] + "kg",
+      abilities: pokemonData.abilities.map((pokemon: { ability: { name: string; }; }) => pokemon.ability.name[0].toUpperCase() + pokemon.ability.name.slice(1, pokemon.ability.name.length)),
+      elements: pokemonData.types.map((pokemon: { type: { name: string | any[]; }; }) => pokemon.type.name[0].toUpperCase() + pokemon.type.name.slice(1, pokemon.type.name.length)),
+      evolutions: this.extractEvolutions(evolutionChain.chain)
+    }
+
+    console.log(evolutionChain);
+
+
+      console.log(this.pokemon);
+      console.log(this.extractEvolutions(evolutionChain.chain));
     },
     error: err => console.error(err)
   });
@@ -142,15 +159,26 @@ export class PokeCardComponent implements OnInit, OnDestroy {
 
 
 
-
-
 extractEvolutions(chain: any) {
-  const evoNames: string[] = [];
+  const evoNames: object[] = [];
   traverse(chain);
   function traverse(node: any) {
     const evoNameBeginUpperCase = node.species.name[0].toUpperCase() + node.species.name.slice(1, node.species.name.length);
-    evoNames.push(evoNameBeginUpperCase);
+
+
+
+    const pokemonIndexId = node.species.url
+      .split('pokemon-species/')[1]
+      .slice(0, node.species.url.split('pokemon-species/')[1].length - 1);
+    const convertetUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemonIndexId}.png`;
+
+    evoNames.push({
+      name: evoNameBeginUpperCase,
+      img: convertetUrl
+    });
+
     node.evolves_to.forEach((child: any) => traverse(child));
+
   };
   return evoNames;
 }
